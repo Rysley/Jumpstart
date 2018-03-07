ef32a93380797fb99dfe44c2cdb7bfb849c4c334
RysLey <ryszardleykam@gmail.com>
2018-03-07 16:15:15 +0100

    responsive + carousel + zoom in

---
+++ resources/JS/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $(".carousel").slick({
+    autoplay: true,
+    autopleySpeed: 4000,
+    slidesToShow: 4,
+    arrows: false,
+    swipe: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
