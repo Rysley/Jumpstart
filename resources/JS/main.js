8492ed0faaa3009eeb052a9bbee56e87977437e1
RysLey <ryszardleykam@gmail.com>
2018-03-07 23:17:03 +0100

    JQuery + minor fix

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,4 +1,4 @@
-
+/*
 $(document).ready(function(){
   $(".carousel").slick({
     autoplay: true,
@@ -9,4 +9,20 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
-});
+});*/
+$(document).ready(function(){
+
+    var lastScrollTop = 0;
+    $(window).scroll(function(){
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop - lastScrollTop > 50){
+        var navHeight = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + navHeight}, 150);
+        lastScrollTop = scrollTop;
+      } else if (lastScrollTop - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        lastScrollTop = scrollTop;
+      }
+    });
+
+  });
